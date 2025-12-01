20af3385fb8df79d5f5a1eef5ed2d6ada6dc4452
johitthebe <np03cs4a230308@heraldcollege.edu.np>
2025-12-02 00:09:17 +0545

    role user type added

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from accounts.models import User
+from accounts.models import User  # your custom user model
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -7,12 +7,12 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'email', 'username', 'password', 'role']
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'role']  # role is set later by choose-role
 
     def create(self, validated_data):
+        user = User(**validated_data, role=role)
         password = validated_data.pop('password')
-        role = validated_data.pop('role')  # <- make sure role is used
-        user = User(**validated_data, role=role)  # set role here
+        user = User(**validated_data)
         user.set_password(password)
         user.save()
         return user
