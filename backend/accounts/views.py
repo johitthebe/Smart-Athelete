794051f001ff5f344912afe81c635d12a8ae1631
johit thebe <np03cs4a230308@heraldcollege.edu.np>
2025-12-18 15:50:05 +0545

    default page

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
-from .serializers import UserSignupSerializer
+from .serializers import UserSerializer
 
 
 @api_view(["POST"])
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from .view import home  
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("accounts/", include("allauth.urls")),  
+    path("api/accounts/", include("accounts.urls")),
 ]
