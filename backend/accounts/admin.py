5d5c4815e861fda7e157f476f4114d479ede2b0d
johit thebe <np03cs4a230308@heraldcollege.edu.np>
2025-12-22 07:51:20 +0545

    admin pannel

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# accounts/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "is_staff", "is_superuser", "is_active")
+    search_fields = ("username", "email")
+
